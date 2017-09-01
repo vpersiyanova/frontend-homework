1123c1e3e7329ab0126d0b0ca2fecac116376627
vpersiyanova <persiyanova.v@yandex.ru>
2017-09-01 09:55:30 +0300

    Add files via upload

--- source/sorting.js
+++ source/sorting.js
@@ -1,32 +1,41 @@
 'use strict';
+
 const sorting = function(arrayOfObj, arrayOfProp) {
-  function fsort(a, b) {
-    if (typeof(a[currentProp]) != "number") {
-      return a[currentProp].localeCompare(b[currentProp]);
-    }
-    return a[currentProp]-b[currentProp];
-  };
-  if (arrayOfProp.length == 0) return arrayOfObj;
-  let currentProp = arrayOfProp[0];
-  arrayOfObj.sort(fsort);
-  let i = 1;
-  while(i < arrayOfProp.length) {
-    let previousProp = currentProp;
-    currentProp = arrayOfProp[i];
+
+  let propSort = () => {
     let delim = 0;
+
     for(let j = 1; j <= arrayOfObj.length; j++) {
-      if (j == arrayOfObj.length || arrayOfObj[j-1][previousProp] != arrayOfObj[j][previousProp]) {
+      if (j === arrayOfObj.length || arrayOfObj[j-1][previousProp] != arrayOfObj[j][previousProp]) {
          let bufArr = arrayOfObj.slice(delim, j);
          if (bufArr.length != 1) {
            sorting(bufArr, [currentProp]);
          }
-         for (let k = 0; k < bufArr.length; k++) {
-            arrayOfObj.splice(delim + k, 1, bufArr[k]);
-         }
+         bufArr.forEach(function(item, i, bufArr){
+            arrayOfObj.splice(delim + i, 1, item);
+         });
          delim = j;
       }
     }
-    i++;
-  };
+  }
+
+  if (arrayOfProp.length === 0) {
+    return arrayOfObj;
+  }
+  let currentProp = arrayOfProp[0];
+
+  arrayOfObj.sort(function (a, b) {
+    if (typeof(a[currentProp]) != "number") {
+      return a[currentProp].localeCompare(b[currentProp]);
+    }
+    return a[currentProp] - b[currentProp];
+  });
+
+  let previousProp = currentProp;
+  arrayOfProp.shift();
+  currentProp = arrayOfProp[0];
+
+  arrayOfProp.forEach(propSort);
+
   return arrayOfObj;
 };
