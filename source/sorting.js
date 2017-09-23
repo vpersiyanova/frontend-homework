e947ce2e79c72edea5cdc5faa219ec0b98cd55d4
vpersiyanova <persiyanova.v@yandex.ru>
2017-09-23 11:53:42 +0300

    new solution

--- source/sorting.js
+++ source/sorting.js
@@ -1,41 +1,22 @@
 'use strict';
 
-const sorting = function(arrayOfObj, arrayOfProp) {
-
-  let propSort = () => {
-    let delim = 0;
-
-    for(let j = 1; j <= arrayOfObj.length; j++) {
-      if (j === arrayOfObj.length || arrayOfObj[j-1][previousProp] != arrayOfObj[j][previousProp]) {
-         let bufArr = arrayOfObj.slice(delim, j);
-         if (bufArr.length != 1) {
-           sorting(bufArr, [currentProp]);
-         }
-         bufArr.forEach(function(item, i, bufArr){
-            arrayOfObj.splice(delim + i, 1, item);
-         });
-         delim = j;
+const sorting = (arrayOfObj, arrayOfProp) => {
+  let sortFunc = (a, b) => {
+    if (a[previousProp] === b[previousProp]) {
+      if (typeof(a[currentProp]) != "number") {
+        return a[currentProp].localeCompare(b[currentProp]);
       }
+      return a[currentProp] - b[currentProp];
     }
+    return -1;
   }
 
-  if (arrayOfProp.length === 0) {
-    return arrayOfObj;
-  }
-  let currentProp = arrayOfProp[0];
+  let currentProp, previousProp;
 
-  arrayOfObj.sort(function (a, b) {
-    if (typeof(a[currentProp]) != "number") {
-      return a[currentProp].localeCompare(b[currentProp]);
-    }
-    return a[currentProp] - b[currentProp];
-  });
-
-  let previousProp = currentProp;
-  arrayOfProp.shift();
-  currentProp = arrayOfProp[0];
-
-  arrayOfProp.forEach(propSort);
-
+  arrayOfProp.forEach(function(item, i, arrayOfProp) {
+    currentProp = item;
+    previousProp = arrayOfProp[i-1];
+    arrayOfObj.sort(sortFunc);
+  })
   return arrayOfObj;
-};
+}
